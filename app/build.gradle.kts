d4b51de963323f2b56b81596f55bb3a32fcda344
tmcwky <tmcwky@nomail.com>
2025-06-14 10:45:09 +0200

    removed google play and firebase stats

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("com.google.gms.google-services") version "4.3.15" apply false
 }
 
 android {
@@ -9,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.rmdssoe.sos"
-        minSdk = 29
+        minSdk = 17
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -33,9 +32,6 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
-    implementation("com.google.android.gms:play-services-location:21.0.1")
-    implementation(platform("com.google.firebase:firebase-bom:32.2.0"))
-    implementation("com.google.firebase:firebase-analytics-ktx")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
